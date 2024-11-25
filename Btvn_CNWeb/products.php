906bcbd09778058a57df6f21d1fc67cd4b3fb1b1
Try_Hng <127312878+TranHoang2409@users.noreply.github.com>
2024-11-25 23:51:29 +0700

    btvn

---
+++ Btvn_CNWeb/products.php
@@ -0,0 +1,21 @@
+<?php
+$products = array (
+  0 => 
+  array (
+    'name' => 'Giày',
+    'price' => '10000',
+    'image' => 'img/h4.webp',
+  ),
+  1 => 
+  array (
+    'name' => 'Áo',
+    'price' => '23000',
+    'image' => 'img/h3.webp',
+  ),
+  2 => 
+  array (
+    'name' => 'Hoàng',
+    'price' => '100000000000000000000000000000000000',
+    'image' => 'img/IMG_1455.JPG',
+  ),
+);
